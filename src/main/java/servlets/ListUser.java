b94f67d4b9f82d8cffdeca3b05399c5d37b21d78
stevehunn <33178230+Stevehunn@users.noreply.github.com>
2021-02-28 15:06:40 +0100

    ajout de commentaire sur les partie non fonctionnel

--- src/main/java/servlets/ListUser.java
+++ src/main/java/servlets/ListUser.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpSession;
 import java.util.HashSet;
 
+//Partie de code non fait
+
 @WebServlet(name = "listuser", value = "/listuser")
 
 public class ListUser extends HttpServlet {
--- src/main/java/servlets/Page1.java
+++ src/main/java/servlets/Page1.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+//pas sur de l'efficacité
 @WebServlet(name = "page1", value = "/page1")
 
 public class Page1 extends HttpServlet {
